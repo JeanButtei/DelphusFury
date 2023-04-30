541704ef160f8a2f2f2f0881563b46693e39d4b2
JeanButtei <jeanbuttei@gmail.com>
2023-04-30 18:48:45 -0300

    Initial

---
+++ src/main/java/br/com/buttler/delphusfury/DelphusFuryApplication.java
@@ -0,0 +1,13 @@
+package br.com.buttler.delphusfury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelphusFuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelphusFuryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/buttler/delphusfury/DelphusFuryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buttler.delphusfury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelphusFuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
